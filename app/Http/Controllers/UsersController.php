86f6b2941c2d84e716027f90e8e6ef29e8e440d8
Vince Millora <vcmillora@up.edu.ph>
2019-04-25 17:20:33 +0900

    Initial

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,9 +2,115 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $users = DB::table('User')
+            ->get(); 
+
+        return view('users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_username'=>'required|string',
+            'user_name'=> 'required|string',
+            'password' => 'required|string'
+        ]);
+        $user = new User([
+            'user_username' => $request->get('user_username'),
+            'user_name'=> $request->get('user_name'),
+            'password'=> $request->get('password')
+        ]);
+        $user->save();
+        return redirect('/users')->with('success', 'New user has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'user_name'=>'required|string',
+            'user_username'=> 'required|string'
+        ]);
+
+        $user = User::find($id);
+        $user->user_name = $request->get('user_name');
+        $user->user_username = $request->get('user_username');
+        $user->save();
+
+        return redirect('/users')->with('success', $request->get('user_username').' has been updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect('/users')->with('success', $id.' has been deleted');
+    }
 }
